114dc8b01e34e39cea9d08f2542774ed8e28c993
roro <2659487705@qq.com>
2017-07-11 20:38:26 +0800

    禁止用户拖动导航头放大页面

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
- <template>
+<template>
   <div id="app">
     <m-header></m-header>
     <tab></tab>
@@ -13,6 +13,11 @@
   import MHeader from 'components/m-header/m-header'
   import Tab from 'components/tab/tab.vue'
   import Player from 'components/player/player'
+
+  // 禁止用户拖动导航头放大页面
+  document.addEventListener('touchmove', function (e) {
+    e.preventDefault()
+  }, false)
 
   export default {
     components: {
